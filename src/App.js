4c3e61a9db3a782c311e74cd6afb9f53cfaffddf
mostafa hassan <mostafa813765@gmail.com>
2021-08-26 22:02:25 +0200

    add sign in component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 import HomePage from "./pages/home/homepage.component";
 import ShopPage from "./pages/shop/shoppage.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exect path="/shop" component={ShopPage} />
+        <Route exect path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
